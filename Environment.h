68de5f1e9de27ebd7b6782ef43e09a1c89f7258b
Charoenchaipiyakul Pavares <a1468869@unet.univie.ac.at>
2016-11-24 14:51:02 +0100

    Finalized c++ version
    like the name

--- Environment.h
+++ Environment.h
@@ -10,11 +10,11 @@
 /*
 * Environment class (or so-called Genetic-Algorithm class) serves as the control sequence of the population.
 * Think of this as the interface controlling when the population will evaluate,
-* cross over, or mutate. That is, natural selection is the effect from environment to 
+* cross over, or mutate. That is, natural selection is the effect from environment to
 * the living organism.
 *
-* At the same time, Environment encapsulates the chromosomes in the swarm, 
-* so that the client program (main program) does not directly access or mess 
+* At the same time, Environment encapsulates the chromosomes in the swarm,
+* so that the client program (main program) does not directly access or mess
 * with the interior data
 */
 class Environment {
@@ -28,6 +28,7 @@ public:
 	~Environment() { delete crowd; } // Class destructor
 
 	void natSelection(int cycleCount, int tmSize, bool isElitist, double mutProb) {
+
 		while (crowd->evaluate() != 0 && --cycleCount >= 0) {
 			crowd->populationSelection(tmSize, isElitist);
 			crowd->crossover(crowd->getSize());
@@ -36,7 +37,7 @@ public:
 	}
 
 	// return answer to the n-queen puzzle as an dynamic integer array (Genes of the best individual)
-	int* getAnswer() { 
+	int* getAnswer() {
 		if (crowd->getBest()->getFitness() == 0)
 			return crowd->getBest()->getGenes();
 		else
--- Population.h
+++ Population.h
@@ -1,108 +1,44 @@
+#ifndef ParaException
+#define ParaException
+
+class ParameterAgainstNatureException : public std::exception {
+public:
+	virtual const char* what() const noexcept override { 
+		return "Something goes against nature. Object cannot be casted.";
+	}
+};
+#endif // ParaException
+
 #ifndef Population_H
 #define Population_H
 
 #include <iostream>
 #include <cstdlib>
 #include <time.h>
-#include "Individual.h"
 
-class Population{
+class Population {
+	/*
+	* note that the chromosomes should be declared in the derived class, due to the various approaches
+	* in various problems, we cannot generalize chromosome type in the mother abstract class
+	*/
+
+	int size; // population size, number of chromosomes in the system
 public:
-	int size; //population size
-	Individual * best; //individual with the best fitness
-	Individual ** individuals; //vector of individuals
-	Individual ** buffer; // buffer for individuals
+	// class constructor
+	Population(int size) {
+		if (size <= 0) throw ParameterAgainstNatureException();
 
-	// Class constructor
-	Population(int size, int ngenes) {
-		this->size = size;
-		individuals = new Individual * [size];
-		buffer = new Individual * [size];
-		
-		for(int i=0; i<size; i++) {
-			individuals[i]= new Individual(ngenes);
-			buffer[i]= new Individual(ngenes);
-		}
-	}
+		this->size = size; 
+	} 
+	virtual ~Population() { }
 
-	// Class destructor
-	~Population() {
-		for(int i=0; i<size; i++)
-			delete individuals[i];
-		
-		delete [] individuals;
-		delete [] buffer;
-	}
+	int getSize() const { return size; }
+	void setSize(int newSize) { size = newSize; }
 
-	int evaluation();
-	Individual* tournamentSelection(int tmSize);
-	void popSelection(int tmSize, bool elitist);
-	void crossover(int count);
-	void mutation(double probability);
-	
+	// pure virtual functions awaits implementation
+	virtual int evaluate() = 0;
+	virtual void crossover(int count) = 0;
+	virtual void mutate(double prob) = 0;
 };
-
-int Population::evaluation(){
-	Individual *bestIndividual = individuals[0];
-	bestIndividual->evaluation();
-
-	for(int i=0; i<size; i++){
-		individuals[i]->evaluation();
-		if(individuals[i]->fitness <= bestIndividual->fitness){
-			bestIndividual = individuals[i];
-		}
-	}
-
-	this->best = bestIndividual;
-	return best->fitness;
-}
-
-Individual* Population::tournamentSelection(int tmSize) {
-	Individual *compIndiv, *tournamentBest;
-
-	tournamentBest = this->individuals[(int)(rand()%size)];
-	while(--tmSize > 0) {
-		compIndiv = this->individuals[(int)(rand()%size)];
-		if(tournamentBest->fitness > compIndiv->fitness)
-			tournamentBest = compIndiv;
-	}
-
-	return tournamentBest;
-}
-
-void Population::popSelection(int tmSize, bool elitist) {
-	int i = this->size-1;
-	//Individual **p;
-
-	if(elitist) {
-		//buffer[i]->copyFrom(best);
-		buffer[i] = best;
-	}
-	while(--i >= 0)
-		buffer[i] = tournamentSelection(tmSize);
-	/*p = individuals;
-	individuals = buffer;
-	buffer = p;
-	best = NULL;*/
-}
-
-void Population::crossover(int count){
-	/* The last element of the array individuals can't take part
-	 in crossover, which means that the second last element
-	 can't be the "left" element in the crossover. 
-	 To make sure that this also applies to odd sizes 
-	 count can't be greater than size-2 */
-
-	if(count > this->size-2)
-		count = this->size-2;
-	
-	for(int i=0; i<count; i+=2)
-		individuals[i]->crossover(individuals[i+1]);
-}
-
-void Population::mutation(double probability){
-	for(int i=0; i<this->size-1; i++)
-		individuals[i]->mutation(probability);
-}
 
 #endif // Population_H
