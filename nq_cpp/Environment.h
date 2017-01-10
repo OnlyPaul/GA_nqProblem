bece1a0fb0046487de9fc44ff3dd9a4cb0ce8b09
OnlyPaul <pavares.cha@gmail.com>
2017-01-10 12:43:46 +0100

    create timing operation

--- nq_cpp/Environment.h
+++ nq_cpp/Environment.h
@@ -4,8 +4,11 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
-#include <time.h>
+#include <ctime>
+#include <chrono>
 #include "NQPopulation.h"
+
+//using namespace std::chrono;
 
 /*
 * Environment class (or so-called Genetic-Algorithm class) serves as the control sequence of the population.
@@ -28,11 +31,15 @@ public:
 	~Environment() { delete crowd; } // Class destructor
 
 	void natSelection(int cycleCount, int tmSize, bool isElitist, double mutProb) {
-
 		while (crowd->evaluate() != 0 && --cycleCount >= 0) {
+			std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
 			crowd->populationSelection(tmSize, isElitist);
 			crowd->crossover(crowd->getSize());
 			crowd->mutate(mutProb);
+			std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
+
+			auto elapse = std::chrono::duration_cast<std::chrono::microseconds>( end - start ).count();
+			std::cout << elapse << std::endl;;
 		}
 	}
 
